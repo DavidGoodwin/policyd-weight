febbe8a2284ff8f068f5f42b7ab286338e5592e1
David Goodwin <david@palepurple.co.uk>
2013-12-18 11:58:27 +0000

    update tests - add something when the sender is missing

--- tests/no-sender.sh
+++ tests/no-sender.sh
@@ -1,7 +1,9 @@
 rm -Rf /tmp/.policyd-weight
+
+
 echo "helo_name=mail.example.org
 recipient=test@example.org
-sender=test@example.org
+sender=
 client_address=8.8.8.8
 request=smtpd_access_policy
-" | perl ../src/policyd-weight.pl -d --no-cache | grep --color -E '^|action=550'
+" | perl ../src/policyd-weight.pl -d --no-cache -f policyd-config-geoip.conf 2>&1 | grep -i -E 'decided action|weighted check'
